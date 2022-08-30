d782fc11d407c067e08433ba1bdf1457cbdf3590
Andi Bellstedt <belltedt@gmx.de>
2022-08-30 23:38:39 +0200

    add tepp to Set-TANSSTicket function

--- PSTANSS/internal/tepp/assignment.ps1
+++ PSTANSS/internal/tepp/assignment.ps1
@@ -9,5 +9,16 @@ Register-PSFTeppArgumentCompleter -Command New-TANSSTicket -Parameter Department
 Register-PSFTeppArgumentCompleter -Command New-TANSSTicket -Parameter EmployeeAssigned -Name PSTANSS.CacheLookup.Employees
 Register-PSFTeppArgumentCompleter -Command New-TANSSTicket -Parameter EmployeeTicketAdmin -Name PSTANSS.CacheLookup.Employees
 Register-PSFTeppArgumentCompleter -Command New-TANSSTicket -Parameter Phase -Name PSTANSS.CacheLookup.Phases
-Register-PSFTeppArgumentCompleter -Command New-TANSSTicket -Parameter Status -Name PSTANSS.CacheLookup.Status
+Register-PSFTeppArgumentCompleter -Command New-TANSSTicket -Parameter Status -Name PSTANSS.CacheLookup.TicketStates
 Register-PSFTeppArgumentCompleter -Command New-TANSSTicket -Parameter Type -Name PSTANSS.CacheLookup.TicketTypes
+Register-PSFTeppArgumentCompleter -Command New-TANSSTicket -Parameter OrderBy -Name PSTANSS.CacheLookup.OrderBys
+
+Register-PSFTeppArgumentCompleter -Command Set-TANSSTicket -Parameter Company -Name PSTANSS.CacheLookup.Companies
+Register-PSFTeppArgumentCompleter -Command Set-TANSSTicket -Parameter Client -Name PSTANSS.CacheLookup.Client
+Register-PSFTeppArgumentCompleter -Command Set-TANSSTicket -Parameter Department -Name PSTANSS.CacheLookup.Departments
+Register-PSFTeppArgumentCompleter -Command Set-TANSSTicket -Parameter EmployeeAssigned -Name PSTANSS.CacheLookup.Employees
+Register-PSFTeppArgumentCompleter -Command Set-TANSSTicket -Parameter EmployeeTicketAdmin -Name PSTANSS.CacheLookup.Employees
+Register-PSFTeppArgumentCompleter -Command Set-TANSSTicket -Parameter Phase -Name PSTANSS.CacheLookup.Phases
+Register-PSFTeppArgumentCompleter -Command Set-TANSSTicket -Parameter Status -Name PSTANSS.CacheLookup.TicketStates
+Register-PSFTeppArgumentCompleter -Command Set-TANSSTicket -Parameter Type -Name PSTANSS.CacheLookup.TicketTypes
+Register-PSFTeppArgumentCompleter -Command Set-TANSSTicket -Parameter OrderBy -Name PSTANSS.CacheLookup.OrderBys
