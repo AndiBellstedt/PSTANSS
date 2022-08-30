43ab56031135f1e97ad9415e4e4a3de326f7190a
Andi Bellstedt <belltedt@gmx.de>
2022-08-30 23:37:09 +0200

    add lookup for linkTypes

--- library/PSTANSS/PSTANSS/Core/TANSS.Lookup.cs
+++ library/PSTANSS/PSTANSS/Core/TANSS.Lookup.cs
@@ -7,59 +7,64 @@ namespace TANSS {
     [Serializable]
     public static class Lookup {
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public static System.Collections.Hashtable Companies = new System.Collections.Hashtable();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public static System.Collections.Hashtable Tickets = new System.Collections.Hashtable();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public static System.Collections.Hashtable TicketStates = new System.Collections.Hashtable();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public static System.Collections.Hashtable TicketTypes = new System.Collections.Hashtable();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public static System.Collections.Hashtable Departments = new System.Collections.Hashtable();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public static System.Collections.Hashtable Employees = new System.Collections.Hashtable();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public static System.Collections.Hashtable Contracts = new System.Collections.Hashtable();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public static System.Collections.Hashtable Phases = new System.Collections.Hashtable();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public static System.Collections.Hashtable CostCenters = new System.Collections.Hashtable();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public static System.Collections.Hashtable OrderBys = new System.Collections.Hashtable();
 
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public static System.Collections.Hashtable Tags = new System.Collections.Hashtable();
+
+        /// <summary>
+        ///
+        /// </summary>
+        public static System.Collections.Hashtable LinkTypes = new System.Collections.Hashtable();
 
     }
 }
